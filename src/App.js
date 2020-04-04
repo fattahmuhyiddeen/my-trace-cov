4bad8081a32ff5c37b8f2e3ffeceb5fbe6c0ccbe
Jun <jjkoh95@gmail.com>
2020-04-05 04:03:37 +0800

    Add in background tasks

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import { PersistGate } from 'redux-persist/integration/react'
 import { Navigator } from '@routes'
 import { store, persistor } from '@store'
 
+import backgroundTask from './utils/backgroundTask';
+
 const App = () => {
+  backgroundTask(); // TODO
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
---
+++ src/utils/backgroundTask.js
@@ -0,0 +1,74 @@
+import BackgroundTimer from 'react-native-background-timer';
+import { BleManager } from 'react-native-ble-plx';
+import BLEPeripheral from 'react-native-ble-peripheral';
+
+const manager = new BleManager();
+
+// Note - backgroundtimer might only work in android
+const scanDevice = (timeout = 1000) => {
+  console.log('ScanDevice');
+  const scannedDevices = new Set();
+  return new Promise((resolve, reject) => {
+    // Define subscriptions.
+    const subscriptions = { timeout: null };
+    // Function to clear all subscriptions.
+    const clearSubscriptions = () => {
+      manager.stopDeviceScan();
+      if (subscriptions.timeout) {
+        BackgroundTimer.clearTimeout(subscriptions.timeout);
+        subscriptions.timeout = null;
+      }
+    };
+    // Specify timeout if value is larger than 0.
+    if (timeout > 0) {
+      subscriptions.timeout = BackgroundTimer.setTimeout(() => {
+        subscriptions.timeout = null;
+        clearSubscriptions();
+        resolve(Array.from(scannedDevices));
+      }, timeout);
+    }
+
+    // Start scanning.
+    manager.startDeviceScan(null, null, (error, scannedDevice) => {
+      var _a;
+      if (error != null) {
+        clearSubscriptions();
+        reject(error);
+      }
+      if (scannedDevice != null) {
+        scannedDevices.add(scannedDevice);
+      }
+    });
+  });
+};
+
+const startAdvertise = async (serviceUUID, name) => {
+  console.log('StartAdvertising');
+  BLEPeripheral.stop()
+  if (await BLEPeripheral.isAdvertising()) return Promise.resolve('Is advertising');
+  BLEPeripheral.addService(serviceUUID, false);
+  BLEPeripheral.addCharacteristicToService(serviceUUID, serviceUUID, 128, 128, '');
+  BLEPeripheral.setName(name);
+  return BLEPeripheral.start();
+};
+
+const uuidDummy = '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d';
+const scheduleBackgroundProcessingTask = async (serviceUUID=uuidDummy, name='TraceCov') => {
+  const scanDeviceTimer = 5000;
+
+  (async function() {
+    BackgroundTimer.runBackgroundTimer(() => {
+      // TODO: scanDevice response should be written to persistent storage
+      scanDevice().then(console.log);
+      startAdvertise(serviceUUID, name).then(console.log).catch(console.log);
+    }, scanDeviceTimer);
+  }());
+
+  // console.log(BackgroundTimer);
+};
+
+export default async () => {
+  // if backgroundtimer running, do nothing
+  scheduleBackgroundProcessingTask();
+  // BackgroundTimer.stopBackgroundTimer(); //after this call all code on background stop run.
+};
